b47998045024de6fc81c05442eac8d4eabdbff83
Anders Eurenius <aes@spotify.com>
2014-07-06 12:24:52 +0200

    Version 0.1.1: Slightly more regular

--- snakehump.el
+++ snakehump.el
@@ -5,7 +5,7 @@
 ;; Author: Anders Eurenius <aes@spotify.com>
 ;; Created: 2014-07-05
 ;; Keywords: formatting camelcase snakecase
-;; Version: 0.1.0
+;; Version: 0.1.1
 
 ;; This file is not part of GNU Emacs.
 
